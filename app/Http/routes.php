d95fe9cea95171ccbd5a6a5a3bb76bbf9efb4a89
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-02-10 19:58:04 +0800

    Added create btn page & route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,3 +10,8 @@ Route::controllers([
 Route::get('/home', ['as' => '/home', function () {
     return view('user.home');
 }]);
+
+
+Route::get('add/btn/{btn_no}', ['as' => 'add_btn', function () {
+    return view('btn.create');
+}]);
