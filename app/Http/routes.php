f42c6a2d347a903e018906a7cb9e31a730d70069
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-02-11 14:39:04 +0800

    Added record route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,6 +12,31 @@ Route::get('/home', ['as' => '/home', function () {
 }]);
 
 
-Route::get('add/btn/{btn_no}', ['as' => 'add_btn', function () {
-    return view('btn.create');
+// Record Model
+Route::bind('record', function($id) { return App\Record::whereId($id)->first(); });
+Route::resource('record', 'RecordController');
+
+Route::get('add/btn/{btn_no}', ['as' => 'add_btn', function ($btn) {
+    return view('btn.create', compact('btn'));
+}]);
+
+get('record_query/{query}', function ($query)
+{
+    $json = [];
+    $records = App\Record::where('btn', 'LIKE', '%'.$query.'%')->get();
+
+    foreach ($records as $record) {
+        $json['items'][] = array(
+            'title' => 'BTN/Phone Number: ' . $record->btn,
+            'description' => 'Reference Number: ' . $record->reference_no,
+            'html_url' => route('record.show', $record->id)
+        );
+    }
+
+    return $json;
+});
+get('record/show/{record_id}', ['as' => 'show_record', function($record) {
+    $record = Record::find($record);
+
+    return view('btn.show', compact('record'));
 }]);
