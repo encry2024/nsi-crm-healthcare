e427971816dd85c1ec85c6b27718eba7c132ab4f
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-02-17 21:26:33 +0800

    Search box name changed to search_mrn

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -8,17 +8,17 @@
     <div class="ui centered grid">
 
         <div class="row">
-            <div class="fifteen wide column">
+            <div class="fourteen wide column">
                 <div class="ui grid">
                     <div class="sixteen wide column">
                         <br><br><br><br><br><br><br>
                         <div class="ui search">
 
                             <h3>
-                                <i class="phone circular orange inverted icon"></i>
-                                SEARCH BTN / PHONE NUMBER</h3>
+                                <i class="file outline circular orange inverted icon"></i>
+                                Search Medical Record Number</h3>
                             <div class="ui icon huge input" style="width: 100%;">
-                                <input type="text" id="search_btn" class="prompt" style="border-radius: 0.3rem;">
+                                <input type="text" id="search_mrn" class="prompt" style="border-radius: 0.3rem;">
                                 <i class="search orange inverted circular icon"></i>
                             </div>
                             <div class="results"></div>
