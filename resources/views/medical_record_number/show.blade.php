49775ffdf2cc3aa90c27fb7e12d8fd288e3486cb
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-02-17 19:56:07 +0800

    Added show medical record number (patient/lead)

--- resources/views/medical_record_number/show.blade.php
+++ resources/views/medical_record_number/show.blade.php
@@ -7,75 +7,79 @@
 
 
 @section('content')
-    <div class="ui centered grid">
+    <div class="ui padded grid">
 
-        <div class="nine wide column">
-            <div class="twelve wide column grid " >
+        <div class="sixteen wide column grid " >
 
-                @if (count($errors) > 0)
-                    <div class="ui negative message">
-                        <i class="close icon"></i>
-                        <div class="header">
-                            User was not able to save because of the following reason(s):
-                        </div>
-                        <ul class="list">
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
+            @if (count($errors) > 0)
+                <div class="ui negative message">
+                    <i class="close icon"></i>
+                    <div class="header">
+                        User was not able to save because of the following reason(s):
                     </div>
-                @endif
+                    <ul class="list">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
-                @if (Session::has('message'))
-                    <div class="ui success message">
-                        <i class="close icon"></i>
-                        <div class="ui header">
-                            <i class="check circle icon"></i>
-                            <div class="content">
-                                {{ Session::get('message') }}
-                            </div>
+            @if (Session::has('message'))
+                <div class="ui success message">
+                    <i class="close icon"></i>
+                    <div class="ui header">
+                        <i class="check circle icon"></i>
+                        <div class="content">
+                            {{ Session::get('message') }}
                         </div>
                     </div>
-                @endif
+                </div>
+            @endif
 
-                <br>
-                <h3 class="ui header">
-                    <i class="large icons">
-                        <i class="book icon"></i>
-                        <i class="corner add icon"></i>
-                    </i>
-                    <div class="content">
-                        Create record
-                    </div>
-                    <div class="ui divider"></div>
-                </h3>
-
-                <div class="ui grid">
-                    <div class="sixteen wide column">
+            <div class="ui grid">
+                <div class="four wide column">
+                    <div class="ui secondary raised orange segment">
                         <form class="" action="{{ route('record.update', $record->id) }}" method="POST">
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
                             <input type="hidden" name="_method" value="PATCH">
 
                             <div class="ui form">
                                 <div class="field @if($errors->has('btn')) error @endif">
+                                    <label>First name <i class="asterisk icon"></i> </label>
+                                    <div class="ui big left icon input">
+                                        <input type="text" name="first_name" value="{{ $record->first_name }}" placeholder="First name" value="{{ Input::old('first_name') }}">
+                                        <i class="info icon"></i>
+                                    </div>
+                                </div>
+
+                                <div class="field @if($errors->has('btn')) error @endif">
+                                    <label>Last name <i class="asterisk icon"></i> </label>
+                                    <div class="ui big left icon input">
+                                        <input type="text" name="last_name" value="{{ $record->last_name }}" placeholder="Last name" value="{{ Input::old('last_name') }}">
+                                        <i class="info icon"></i>
+                                    </div>
+                                </div>
+
+                                <div class="field @if($errors->has('btn')) error @endif">
                                     <label>BTN/Phone Number <i class="asterisk icon"></i> </label>
-                                    <div class="ui left icon input">
-                                        <input type="text" name="btn" value="{{ $record->btn }}" placeholder="BTN/Phone Number" value="{{ Input::old('btn') }}" readonly>
+                                    <div class="ui big left icon input">
+                                        <input type="text" name="btn" value="{{ $record->btn }}" placeholder="BTN/Phone Number" value="{{ Input::old('btn') }}">
                                         <i class="phone icon"></i>
                                     </div>
                                 </div>
 
                                 <div class="field @if($errors->has('reference_no')) error @endif">
                                     <label>Reference Number <i class="asterisk icon"></i> </label>
-                                    <div class="ui left icon input">
-                                        <input type="text" name="reference_no" value="{{ $record->reference_no }}" placeholder="Reference Number" value="{{ Input::old('btn') }}">
+                                    <div class="ui big left icon input">
+                                        <input type="text" name="reference_no" value="{{ $record->reference_no }}" placeholder="Reference Number" value="{{ Input::old('reference_no') }}">
                                         <i class="file text outline icon"></i>
                                     </div>
                                 </div>
 
                                 <div class="field @if($errors->has('date_of_birth')) error @endif">
                                     <label>Date of Birth <i class="asterisk icon"></i> </label>
-                                    <div class="ui left icon input">
+                                    <div class="ui big left icon input">
                                         <input name="date_of_birth" value="{{ date('F d, Y', strtotime($record->date_of_birth)) }}" placeholder="Date of Birth" id="dob">
                                         <i class="calendar icon"></i>
                                     </div>
@@ -97,20 +101,141 @@
 
                                 <div class="field @if($errors->has('call_notes')) error @endif">
                                     <label>Call Note <i class="asterisk icon"></i> </label>
-                                    <div class="ui left icon input">
+                                    <div class="ui big left icon input">
                                         <textarea name="call_notes">{{ $record->call_notes }}</textarea>
                                         <i class="pencil icon"></i>
                                     </div>
                                 </div>
 
-                                <div class="ui buttons right floated">
-                                    <a class="ui button negative" href="{{ route('/home') }}">Cancel</a>
-                                    <div class="or"></div>
-                                    <button class="ui positive button">Save</button>
-                                </div>
+                                <button class="ui button fluid">Update</button>
                             </div>
 
                         </form>
+                    </div>
+                </div>
+
+                {{--
+                Tab 1 - Demographics
+                Tab 2 - Breast Cancer Screening
+                Tab 3 - Colon Cancer Screening
+                Tab 4 - Flu Vaccination
+                Tab 5 - Pneumonia Vaccination
+                Tab 6 - Blood pressure
+                Tab 7 - Diabetes: A1C
+                Tab 8 - Diabetes: Eye exam
+                Tab 9 - High Risk Meds
+                Tab 10 Other
+                --}}
+
+                <div class="twelve wide column">
+                    <div class="ui grid">
+
+                        <div class="twelve wide stretched column">
+                            <div class="row">
+                                <h2 class="header">
+                                    <div class="content">
+                                        Demographics
+                                    </div>
+                                </h2>
+                            </div>
+
+                            <div class="row">
+                                <div class="ui divider"></div>
+                            </div>
+
+                            <div class="row">
+                                <div class="ui basic segment">
+                                    <form action="" class="ui form">
+                                        <div class="field @if($errors->has('q1')) error @endif">
+                                            <label style="font-size: 16px;">1) Date of most recent office visit</label>
+                                            <div class="ui big left icon input">
+                                                <input name="q1" id="v_d">
+                                                <i class="calendar icon"></i>
+                                            </div>
+                                        </div>
+
+                                        <div class="field @if($errors->has('q2')) error @endif">
+                                            <label for="q2" style="font-size: 16px;">2) Is PCP listed in PCT (patient care team?)</label>
+                                            <div class="ui radio checkbox" style="margin-top: 0.5rem !important;">
+                                                <input type="radio" name="q2" id="q2" checked="checked">
+                                                <label>Yes</label>
+                                            </div>
+                                        </div>
+
+                                        <div class="field @if($errors->has('q2')) error @endif">
+                                            <div class="ui radio checkbox">
+                                                <input type="radio" name="q2" id="q2">
+                                                <label>No</label>
+                                            </div>
+                                        </div>
+
+                                        <div class="field @if($errors->has('q3')) error @endif">
+                                            <label for="q3" style="font-size: 16px;">3) SMG PCP (In Patient Care Team)</label>
+                                            <div class="ui big left input">
+                                                <input name="q3" id="q3">
+                                            </div>
+                                        </div>
+
+                                        <div class="field @if($errors->has('q4')) error @endif">
+                                            <label for="q4" style="font-size: 16px;">4) Did patient Request to NOT be contacted?</label>
+                                            <div class="ui big left input">
+                                                <input name="q4" id="q4">
+                                            </div>
+                                        </div>
+
+                                        <div class="field @if($errors->has('q5')) error @endif">
+                                            <label style="font-size: 16px;">5) Date of Chart review</label>
+                                            <div class="ui big left icon input">
+                                                <input name="q5" id="date_chart_review">
+                                                <i class="calendar icon"></i>
+                                            </div>
+                                        </div>
+
+                                        <div class="ui divider"></div>
+
+                                        <div class="actions">
+                                            <button class="ui button primary fluid">Submit</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="four wide column">
+                            <div class="ui vertical fluid right tabular menu" style="width: 81.5% !important;">
+                                <a class="active item">
+                                    Demographics
+                                </a>
+                                <a class="item" href="{{ route('bcs', $record->id) }}">
+                                    Breast Cancer Screening
+                                </a>
+                                <a class="item" href="{{ route('ccs', $record->id) }}">
+                                    Colon Cancer Screening
+                                </a>
+                                <a class="item" href="{{ route('fv', $record->id) }}">
+                                    Flu Vaccination
+                                </a>
+                                <a class="item" href="{{ route('pv', $record->id) }}">
+                                    Pneumonia Vaccination
+                                </a>
+                                <a class="item" href="{{ route('bp', $record->id) }}">
+                                    Blood pressure
+                                </a>
+                                <a class="item" href="{{ route('da1c', $record->id) }}">
+                                    Diabetes: A1C
+                                </a>
+                                <a class="item" href="{{ route('dee', $record->id) }}">
+                                    Diabetes: Eye Exam
+                                </a>
+                                <a class="item" href="{{ route('hrm', $record->id) }}">
+                                    High Risk Meds
+                                </a>
+                                <a class="item" href="{{ route('o', $record->id) }}">
+                                    Other
+                                </a>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
@@ -122,6 +247,16 @@
 @section('scripts')
     <script type="text/javascript">
         $("#dob").datepicker({
+            format: 'MM dd, yyyy',
+            autoclose: true
+        });
+
+        $("#v_d").datepicker({
+            format: 'MM dd, yyyy',
+            autoclose: true
+        });
+
+        $("#date_chart_review").datepicker({
             format: 'MM dd, yyyy',
             autoclose: true
         });
