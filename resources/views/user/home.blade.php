44ff8713c8b66f9b5d9342ea718114fb12df600c
Lamberto Guevarra <seoblogger07@gmail.com>
2016-03-09 20:36:20 +0800

    update dashboard layout

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -5,146 +5,129 @@
 @stop
 
 @section('content')
-    <div class="ui centered padded grid">
+    <div class="ui relaxed padded grid">
+        <div class="ui doubling two column row">
+            <div class="column">
+                <h2 class="ui header">
+                    <div class="content"><i class="dashboard icon"></i>Dashboard</div>
+                </h2>
+                <div class="ui divider"></div>
 
-        <div class="row">
-            <div class="sixteen wide column">
-                <div class="ui grid">
-                    <br><br>
-                    <div class="ten wide column">
-
-                        <h2 class="ui header">
-                            <div class="content"><i class="dashboard icon"></i>Dashboard</div>
-                        </h2>
-                        <div class="ui divider"></div>
-
-                        <div class="ui grid">
-                        <div class="row">
-                            <div class="ten wide column">
-                                <div class="ui inverted grey segment">
-                                    <form action="{{ route('/home') }}" method="GET" style="margin: 0; !important">
-                                    <div class="ui inverted small form">
-                                        <div class="two fields">
-                                            <div class="field">
-                                                <label> Gender </label>
-                                                <select class="ui dropdown" name="gender">
-                                                    <option value="">All</option>
-                                                    <option {{ request('gender') =='M' ? 'selected':'' }} value="M">Male</option>
-                                                    <option {{ request('gender') =='F' ? 'selected':'' }} value="F">Female</option>
-                                                </select>
-                                            </div>
-                                            <div class="field">
-                                                <label>Age</label>
-                                                <div class="two fields">
-                                                    <div class="field">
-                                                        <input placeholder="from..." type="text" name="age_from" value="{{ request('age_from') }}">
-                                                    </div>
-                                                    <div class="field">
-                                                        <input placeholder="to..." type="text" name="age_to" value="{{ request('age_to') }}">
-                                                    </div>
-                                                </div>
-                                            </div>
+                <div class="ui inverted grey segment">
+                    <form action="{{ route('/home') }}" method="GET" style="margin: 0; !important">
+                        <div class="ui inverted small form">
+                            <div class="two fields">
+                                <div class="field">
+                                    <label> Gender </label>
+                                    <select class="ui dropdown" name="gender">
+                                        <option value="">All</option>
+                                        <option {{ request('gender') =='M' ? 'selected':'' }} value="M">Male</option>
+                                        <option {{ request('gender') =='F' ? 'selected':'' }} value="F">Female</option>
+                                    </select>
+                                </div>
+                                <div class="field">
+                                    <label>Age</label>
+                                    <div class="two fields">
+                                        <div class="field">
+                                            <input placeholder="from..." type="text" name="age_from" value="{{ request('age_from') }}">
                                         </div>
-                                        <button class="ui submit black button">Filter</button>
-                                        <a href="{{ route('/home') }}" class="ui button black">Clear Filter</a>
+                                        <div class="field">
+                                            <input placeholder="to..." type="text" name="age_to" value="{{ request('age_to') }}">
+                                        </div>
                                     </div>
-                                    </form>
                                 </div>
                             </div>
+                            <button class="ui submit button">Filter</button>
+                            <a href="{{ route('/home') }}" class="ui button">Clear Filter</a>
                         </div>
+                    </form>
+                </div>
+                <table class="ui table striped unstackable small">
+                    <thead>
+                    <tr>
+                        <th class="one wide"></th>
+                        <th class="six wide">Patient Name</th>
+                        <th class="three wide">MRN</th>
+                        <th class="one wide">Gender</th>
+                        <th class="one wide">Age</th>
+                        <th class="five wide">Disposition</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($records as $record)
+                        <tr>
+                            <td>{{ ((($records->currentPage() - 1) * $records->perPage()) + ($ctr++) + 1) }}</td>
+                            <td><a style="font-weight: bold" href="{{ route('record.show', $record->id) }}">{{ $record->name }}</a></td>
+                            <td>{{ $record->mrn }}</td>
+                            <td>{{ $record->gender }}</td>
+                            <td>{{ $record->age }}</td>
+                            <td>{{ isset($record->getLastDisposition()->disposition_id)?$record->getLastDisposition()->disposition->name:"" }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                @include('util.paginator', ['paginator' => $records->appends(Request::only('filter'))])
+            </div>
+
+            <div class="column">
+                <div class="row">
+                    <h2 class="ui header">
+                        <div class="content">
+                            Callbacks
                         </div>
-                        <table class="ui compact table striped">
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th>Patient Name</th>
-                                    <th>MRN</th>
-                                    <th>Gender</th>
-                                    <th>Age</th>
-                                    <th>Disposition</th>
-                                    <th>Call Notes</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            @foreach ($records as $record)
-                                <tr>
-                                    <td>{{ ((($records->currentPage() - 1) * $records->perPage()) + ($ctr++) + 1) }}</td>
-                                    <td>{{ $record->name }}</td>
-                                    <td>{{ $record->mrn }}</td>
-                                    <td>{{ $record->gender }}</td>
-                                    <td>{{ $record->age }}</td>
-                                    <td>{{ isset($record->getLastDisposition()->disposition_id)?$record->getLastDisposition()->disposition->name:"" }}</td>
-                                    <td>{{ (strlen($record->call_notes) > 30) ? substr($record->call_notes, 0, 30) . '...' : $record->call_notes }}</td>
-                                    <td><a class="ui button primary small" href="{{ route('record.show', $record->id) }}">view</a></td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                        @include('util.paginator', ['paginator' => $records->appends(Request::only('filter'))])
-                    </div>
+                    </h2>
 
-                    <div class="six wide column">
-                        <div class="row">
-                            <h2 class="ui header">
-                                <div class="content">
-                                    Callbacks
-                                </div>
-                            </h2>
+                    <div class="ui divider"></div>
 
-                            <div class="ui divider"></div>
-
-                            <table class="ui celled striped table">
-                                <thead>
-                                <tr><th colspan="3">
-                                        History
-                                    </th>
-                                </tr></thead>
-                                <tbody>
-                                @foreach($callbacks as $callback)
-                                    <tr>
-                                        <td><a href="{{ route('record.show', $callback->record->id) }}">{{ $callback->record->name }}</a></td>
-                                        <td> {{ $callback->schedule->diffForHumans() }}</td>
-                                        <td>{{ $callback->notes }}</td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                            {{--<button class="ui primary button" id="callback_modal">
-                                Add New Callback
-                            </button>--}}
+                    <table class="ui striped table unstackable small">
+                        <thead>
+                        <tr><th colspan="3">
+                                History
+                            </th>
+                        </tr></thead>
+                        <tbody>
+                        @foreach($callbacks as $callback)
+                            <tr>
+                                <td><a style="font-weight: bold" ref="{{ route('record.show', $callback->record->id) }}">{{ $callback->record->name }}</a></td>
+                                <td> {{ $callback->schedule->diffForHumans() }}</td>
+                                <td>{{ $callback->notes }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                    {{--<button class="ui primary button" id="callback_modal">
+                        Add New Callback
+                    </button>--}}
+                </div>
+                <br>
+                <div class="row">
+                    <h2 class="ui header">
+                        <div class="content">
+                            Recent Records
                         </div>
-                        <br>
-                        <div class="row">
-                            <h2 class="ui header">
-                                <div class="content">
-                                    Recent Records
-                                </div>
-                            </h2>
+                    </h2>
 
-                            <div class="ui divider"></div>
+                    <div class="ui divider"></div>
 
-                            <table class="ui celled striped table">
-                                <thead>
-                                <tr><th colspan="3">
-                                        History
-                                    </th>
-                                </tr></thead>
-                                <tbody>
-                                @foreach(\Illuminate\Support\Facades\Auth::user()->records()->where('updated_at', '!=' ,'0000-00-00 00:00:00')->orderBy('records.updated_at', 'DESC')->take(5)->get() as $record)
-                                    <tr>
-                                        <td><a href="{{ route('record.show', $record->id) }}">{{ $record->name }}</a></td>
-                                        <td>{{ isset($record->getLastDisposition()->disposition_id)?$record->getLastDisposition()->disposition->name:"" }}</td>
-                                        <td> {{ $record->updated_at->diffForHumans() }}</td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                            {{--<button class="ui primary button" id="callback_modal">
-                                Add New Callback
-                            </button>--}}
-                        </div>
-                    </div>
+                    <table class="ui striped table unstackable small">
+                        <thead>
+                        <tr><th colspan="3">
+                                History
+                            </th>
+                        </tr></thead>
+                        <tbody>
+                        @foreach(\Illuminate\Support\Facades\Auth::user()->records()->where('updated_at', '!=' ,'0000-00-00 00:00:00')->orderBy('records.updated_at', 'DESC')->take(5)->get() as $record)
+                            <tr>
+                                <td><a style="font-weight: bold" href="{{ route('record.show', $record->id) }}">{{ $record->name }}</a></td>
+                                <td>{{ isset($record->getLastDisposition()->disposition_id)?$record->getLastDisposition()->disposition->name:"" }}</td>
+                                <td> {{ $record->updated_at->diffForHumans() }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                    {{--<button class="ui primary button" id="callback_modal">
+                        Add New Callback
+                    </button>--}}
                 </div>
             </div>
         </div>
