501372805633f26e4b1b25635a16bf30fe230bbd
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-03-16 20:43:57 +0800

    Reverted back to agent page

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -12,63 +12,61 @@
                     <div class="content"><i class="dashboard icon"></i>Dashboard</div>
                 </h2>
                 <div class="ui divider"></div>
-
-                <div class="ui inverted grey segment">
-                    <form action="{{ route('/home') }}" method="GET" style="margin: 0; !important">
-                        <div class="ui inverted small form">
-                            <div class="two fields">
-                                <div class="field">
-                                    <label> Gender </label>
-                                    <select class="ui dropdown" name="gender">
-                                        <option value="">All</option>
-                                        <option {{ request('gender') =='M' ? 'selected':'' }} value="M">Male</option>
-                                        <option {{ request('gender') =='F' ? 'selected':'' }} value="F">Female</option>
-                                    </select>
-                                </div>
-                                <div class="field">
-                                    <label>Age</label>
-                                    <div class="two fields">
-                                        <div class="field">
-                                            <input placeholder="from..." type="text" name="age_from" value="{{ request('age_from') }}">
-                                        </div>
-                                        <div class="field">
-                                            <input placeholder="to..." type="text" name="age_to" value="{{ request('age_to') }}">
+                    <div class="ui inverted grey segment">
+                        <form action="{{ route('/home') }}" method="GET" style="margin: 0; !important">
+                            <div class="ui inverted small form">
+                                <div class="two fields">
+                                    <div class="field">
+                                        <label> Gender </label>
+                                        <select class="ui dropdown" name="gender">
+                                            <option value="">All</option>
+                                            <option {{ request('gender') =='M' ? 'selected':'' }} value="M">Male</option>
+                                            <option {{ request('gender') =='F' ? 'selected':'' }} value="F">Female</option>
+                                        </select>
+                                    </div>
+                                    <div class="field">
+                                        <label>Age</label>
+                                        <div class="two fields">
+                                            <div class="field">
+                                                <input placeholder="from..." type="text" name="age_from" value="{{ request('age_from') }}">
+                                            </div>
+                                            <div class="field">
+                                                <input placeholder="to..." type="text" name="age_to" value="{{ request('age_to') }}">
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+                                <button class="ui submit button">Filter</button>
+                                <a href="{{ route('/home') }}" class="ui button">Clear Filter</a>
                             </div>
-                            <button class="ui submit button">Filter</button>
-                            <a href="{{ route('/home') }}" class="ui button">Clear Filter</a>
-                        </div>
-                    </form>
-                </div>
-                <table class="ui table striped unstackable small">
-                    <thead>
-                    <tr>
-                        <th class="one wide"></th>
-                        <th class="six wide">Patient Name</th>
-                        <th class="three wide">MRN</th>
-                        <th class="one wide">Gender</th>
-                        <th class="one wide">Age</th>
-                        <th class="five wide">Disposition</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($records as $record)
+                        </form>
+                    </div>
+                    <table class="ui table striped unstackable small">
+                        <thead>
                         <tr>
-                            <td>{{ ((($records->currentPage() - 1) * $records->perPage()) + ($ctr++) + 1) }}</td>
-                            <td><a style="font-weight: bold" href="{{ route('record.show', $record->id) }}">{{ $record->name }}</a></td>
-                            <td>{{ $record->mrn }}</td>
-                            <td>{{ $record->gender }}</td>
-                            <td>{{ $record->age }}</td>
-                            <td>{{ isset($record->getLastDisposition()->disposition_id)?$record->getLastDisposition()->disposition->name:"" }}</td>
+                            <th class="one wide"></th>
+                            <th class="six wide">Patient Name</th>
+                            <th class="three wide">MRN</th>
+                            <th class="one wide">Gender</th>
+                            <th class="one wide">Age</th>
+                            <th class="five wide">Disposition</th>
                         </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-                @include('util.paginator', ['paginator' => $records->appends(Request::only('filter'))])
-            </div>
-
+                        </thead>
+                        <tbody>
+                        @foreach ($records as $record)
+                            <tr>
+                                <td>{{ ((($records->currentPage() - 1) * $records->perPage()) + ($ctr++) + 1) }}</td>
+                                <td><a style="font-weight: bold" href="{{ route('record.show', $record->id) }}">{{ $record->name }}</a></td>
+                                <td>{{ $record->mrn }}</td>
+                                <td>{{ $record->gender }}</td>
+                                <td>{{ $record->age }}</td>
+                                <td>{{ isset($record->getLastDisposition()->disposition_id)?$record->getLastDisposition()->disposition->name:"" }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                    @include('util.paginator', ['paginator' => $records->appends(Request::only('filter'))])
+                </div>
             <div class="column">
                 <div class="row">
                     <h2 class="ui header">
