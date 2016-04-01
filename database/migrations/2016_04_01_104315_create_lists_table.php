e14ad38e8c307b14eba82c349699f6fc717775f2
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-04-01 21:29:41 +0800

    Added create lists table migration

---
+++ database/migrations/2016_04_01_104315_create_lists_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lists', function (Blueprint $blueprint) {
+            $blueprint->increments('id');
+            $blueprint->string('name');
+            $blueprint->timestamps();
+            $blueprint->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('lists');
+    }
+}
