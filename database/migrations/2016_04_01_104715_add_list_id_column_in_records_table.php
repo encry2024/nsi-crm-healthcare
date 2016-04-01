32e5360a8e459d1138e694d8ca1fc3b25f485d4f
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-04-01 21:30:20 +0800

    Added add list id column in records table migration

---
+++ database/migrations/2016_04_01_104715_add_list_id_column_in_records_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddListIdColumnInRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('records', function (Blueprint $table) {
+            $table->integer('list_id')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('records', function (Blueprint $table) {
+            $table->dropColumn('list_id');
+        });
+    }
+}
